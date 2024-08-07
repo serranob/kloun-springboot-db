de266c39474e852a7d9774333938e6548d8a4789
serranob <brenogserrano@hotmail.com>
2024-08-06 21:42:40 -0300

    arrumei cores e limpei item.java

--- src/main/java/com/web2/kloun/Item.java
+++ src/main/java/com/web2/kloun/Item.java
@@ -1,20 +1,5 @@
 package com.web2.kloun;
 
 public abstract class Item {
-    private String nome;
-    private String categoria;
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getCategoria() {
-        return categoria;
-    }
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
     
 }
